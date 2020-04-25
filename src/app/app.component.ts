2bf61c1e13fae872e7369d9ce9076b8651469398
Bhupendra prasad <bhupendP@apples-MacBook-Air.local>
2020-04-25 23:42:26 +0530

    note app implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,98 @@
-import { Component } from '@angular/core';
+import { Component, ChangeDetectorRef, OnInit, AfterViewChecked, ElementRef } from '@angular/core';
+import { FormsModule, FormBuilder, FormGroup, FormControl } from '@angular/forms';
+import { NoteService } from './note.service';
+import { Note } from './note';
+import * as MarkdownIt from 'markdown-it';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements AfterViewChecked, OnInit {
   title = 'notes-app';
+  newNote: Note = new Note();
+  notes = [];
+  noteForm: FormGroup;
+  editNoteForm: FormGroup;
+  previewTitle: string;
+  previewContent: string;
+  showOverlay = false;
+  showDeleteModal = false;
+  noteToDelete = {};
+  noteToEdit = {editing: false};
+  today: number = Date.now();
+  md = new MarkdownIt();
+
+constructor(private noteService: NoteService, private fb: FormBuilder,
+            private elementRef: ElementRef, private cdr: ChangeDetectorRef) {
+  this.createForm();
+  setInterval(() => {this.today = Date.now()}, 1);
+}
+
+ngAfterViewChecked() {
+  this.cdr.detectChanges();
+}
+
+createForm() {
+  this.noteForm = this.fb.group({'title': '', 'content': ''});
+  this.previewTitle = '';
+  this.previewContent = '';
+  this.subscribeChanges();
+}
+
+getNotes() {
+  this.notes = this.noteService.notes;
+}
+
+addNote(note) {
+  this.newNote = note;
+  this.noteService.addNote(this.newNote);
+  this.newNote = new Note();
+}
+
+editNote(note) {
+  this.editNoteForm = this.fb.group({'title': note.title, 'content': note.content});
+  this.showOverlay = true;
+  note.editing = true;
+  this.noteToEdit = note;
+  // Set focus to current note form textarea
+  setTimeout(() => {
+    this.elementRef.nativeElement.querySelector('#content').focus();
+  }, 0);
+
+}
+
+removeNote(note) {
+  console.log(this.noteForm.controls);
+  this.noteForm.controls['title'].setValue('');
+  this.noteForm.controls['content'].setValue('');
+  this.noteToDelete = {};
+  this.getNotes();
+}
+
+subscribeChanges() {
+  this.noteForm.controls['title'].valueChanges.subscribe(value => {
+    this.previewTitle = value;
+  });
+  this.noteForm.controls['content'].valueChanges.subscribe(value => {
+    this.previewContent = this.parseMarkdown(value);
+  });
+}
+
+parseMarkdown(content) {
+  return this.md.render(content);
+}
+
+ngOnInit() {
+  this.getNotes();
+  this.subscribeChanges();
+}
+
+onSubmit(value: any): void {
+  this.addNote(value);
+  // this.createForm();
+}
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatInputModule} from '@angular/material/input';
+import {MatTableModule} from '@angular/material/table';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatMenuModule, MatToolbarModule} from '@angular/material';
+import {MatIconModule} from '@angular/material/icon';
+import {MatListModule} from '@angular/material/list';
+
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
+import { NoteService } from './note.service';
 
 @NgModule({
   declarations: [
@@ -10,9 +26,25 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule,
+    FormsModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    NoopAnimationsModule,
+    MatInputModule,
+    MatTableModule,
+    MatSelectModule,
+    MatCardModule,
+    MatGridListModule,
+    MatMenuModule,
+    MatButtonModule,
+    MatIconModule,
+    MatListModule,
+    MatToolbarModule,
+    MatSidenavModule
   ],
-  providers: [],
+  providers: [NoteService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
